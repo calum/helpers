runelite/runelite

Add option to always draw untradeable items
Instead of checking for GE price being above 0, use isTradeable boolean
to determine if the price checks should apply or not and add
configuration value to disable this behaviour.

Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsOverlay.java
@@ -131,12 +131,18 @@ public class GroundItemsOverlay extends Overlay
 				item.setGePrice(itemPrice.getPrice() * item.getQuantity());
 			}
 
-			// Do not display items that are under HA or GE price and are not highlighted
-			if (!plugin.isHotKeyPressed() && !highlighted
-				&& ((item.getGePrice() > 0 && item.getGePrice() < config.getHideUnderGeValue())
-				|| item.getHaPrice() < config.getHideUnderHAValue()))
+			if (!plugin.isHotKeyPressed() && !highlighted)
 			{
-				continue;
+				// Check if item is under config threshold
+				final boolean underThreshold = item.getGePrice() < config.getHideUnderGeValue()
+					|| item.getHaPrice() < config.getHideUnderHAValue();
+
+				// If item is under threshold an we are either not always showing untradeables or item is tradeable
+				// do not display item
+				if (underThreshold && (!config.alwaysShowUntradeable() || item.isTradeable()))
+				{
+					continue;
+				}
 			}
 
 			final Color color = getCostColor(item.getGePrice() > 0 ? item.getGePrice() : item.getHaPrice(),

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsOverlay.java
@@ -138,12 +138,18 @@ public class GroundItemsOverlay extends Overlay
 				item.setGePrice(itemPrice.getPrice() * item.getQuantity());
 			}
 
-			// Do not display items that are under HA or GE price and are not highlighted
-			if (!plugin.isHotKeyPressed() && !highlighted
-				&& ((item.getGePrice() > 0 && item.getGePrice() < config.getHideUnderGeValue())
-				|| item.getHaPrice() < config.getHideUnderHAValue()))
+			if (!plugin.isHotKeyPressed() && !highlighted)
 			{
-				continue;
+				// Check if item is under config threshold
+				final boolean underThreshold = item.getGePrice() < config.getHideUnderGeValue()
+					|| item.getHaPrice() < config.getHideUnderHAValue();
+
+				// If item is under threshold an we are either not always showing untradeables or item is tradeable
+				// do not display item
+				if (underThreshold && (!config.dontHideUntradeables() || item.isTradeable()))
+				{
+					continue;
+				}
 			}
 
 			final Color color = getCostColor(item.getGePrice() > 0 ? item.getGePrice() : item.getHaPrice(),