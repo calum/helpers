open-osrs/runelite

interfacestyles: add ability to always stack resizable stones

runelite-client/src/main/java/net/runelite/client/plugins/interfacestyles/InterfaceStylesConfig.java
@@ -72,4 +72,14 @@ public interface InterfaceStylesConfig extends Config
 	{
 		return false;
 	}
+
+	@ConfigItem(
+		keyName = "alwaysStack",
+		name = "Always stack bottom bar",
+		description = "Always stack the bottom bar in resizable"
+	)
+	default boolean stack()
+	{
+		return false;
+	}
 }

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/interfacestyles/InterfaceStylesConfig.java
@@ -72,4 +72,14 @@ public interface InterfaceStylesConfig extends Config
 	{
 		return false;
 	}
+
+	@ConfigItem(
+		keyName = "alwaysStack",
+		name = "Always stack bottom bar",
+		description = "Always stack the bottom bar in resizable"
+	)
+	default boolean alwaysStack()
+	{
+		return false;
+	}
 }