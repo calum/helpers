Adam-/runelite

client: consistently capitalize config descriptions

runelite-client/src/main/java/net/runelite/client/plugins/specialcounter/SpecialCounterConfig.java
@@ -38,7 +38,7 @@ public interface SpecialCounterConfig extends Config
 		position = 0,
 		keyName = "thresholdNotification",
 		name = "Threshold notifications",
-		description = "Sends a notification when your special attack counter exceeds the threshold"
+		description = "Sends a notification when your special attack counter exceeds the threshold."
 	)
 	default Notification thresholdNotification()
 	{
@@ -49,7 +49,7 @@ public interface SpecialCounterConfig extends Config
 		position = 1,
 		keyName = "specDrops",
 		name = "Spec drops",
-		description = "Draws an overlay over the player when a special attack hits"
+		description = "Draws an overlay over the player when a special attack hits."
 	)
 	default boolean specDrops()
 	{
@@ -60,7 +60,7 @@ public interface SpecialCounterConfig extends Config
 		position = 1,
 		keyName = "specDropMisses",
 		name = "Spec drop misses",
-		description = "Draws an overlay over the player when a special attack misses"
+		description = "Draws an overlay over the player when a special attack misses."
 	)
 	default boolean specDropMisses()
 	{
@@ -71,7 +71,7 @@ public interface SpecialCounterConfig extends Config
 		position = 2,
 		keyName = "specDropColor",
 		name = "Spec drop color",
-		description = "Text color for spec drops"
+		description = "Text color for spec drops."
 	)
 	default Color specDropColor()
 	{
@@ -82,7 +82,7 @@ public interface SpecialCounterConfig extends Config
 		position = 3,
 		keyName = "infobox",
 		name = "Infobox",
-		description = "Adds an infobox counting special attacks"
+		description = "Adds an infobox counting special attacks."
 	)
 	default boolean infobox()
 	{
@@ -93,7 +93,7 @@ public interface SpecialCounterConfig extends Config
 		position = 3,
 		keyName = "defenceDrainInfobox",
 		name = "Show defence drain infobox",
-		description = "If infoboxes are enabled, adds a defence drain percentage infobox for Dragon warhammer and Elder maul"
+		description = "If infoboxes are enabled, adds a defence drain percentage infobox for dragon warhammer and elder maul."
 	)
 	default boolean defenceDrainInfobox()
 	{
@@ -104,7 +104,7 @@ public interface SpecialCounterConfig extends Config
 		position = 10,
 		keyName = "dragonWarhammerThreshold",
 		name = "Dragon warhammer",
-		description = "Threshold for Dragon Warhammer (0 to disable)"
+		description = "Threshold for dragon warhammer (0 to disable)."
 	)
 	default int dragonWarhammerThreshold()
 	{
@@ -115,7 +115,7 @@ public interface SpecialCounterConfig extends Config
 		position = 15,
 		keyName = "elderMaulThreshold",
 		name = "Elder maul",
-		description = "Threshold for Elder Maul (0 to disable)"
+		description = "Threshold for elder maul (0 to disable)."
 	)
 	default int elderMaulThreshold()
 	{
@@ -126,7 +126,7 @@ public interface SpecialCounterConfig extends Config
 		position = 20,
 		keyName = "arclightThreshold",
 		name = "Arclight",
-		description = "Threshold for Arclight (0 to disable)"
+		description = "Threshold for arclight (0 to disable)."
 	)
 	default int arclightThreshold()
 	{
@@ -137,7 +137,7 @@ public interface SpecialCounterConfig extends Config
 		position = 30,
 		keyName = "darklightThreshold",
 		name = "Darklight",
-		description = "Threshold for Darklight (0 to disable)"
+		description = "Threshold for darklight (0 to disable)."
 	)
 	default int darklightThreshold()
 	{
@@ -148,7 +148,7 @@ public interface SpecialCounterConfig extends Config
 		position = 31,
 		keyName = "emberlightThreshold",
 		name = "Emberlight",
-		description = "Threshold for Emberlight (0 to disable)"
+		description = "Threshold for emberlight (0 to disable)."
 	)
 	default int emberlightThreshold()
 	{
@@ -159,7 +159,7 @@ public interface SpecialCounterConfig extends Config
 		position = 40,
 		keyName = "bandosGodswordThreshold",
 		name = "Bandos godsword",
-		description = "Threshold for Bandos Godsword (0 to disable)"
+		description = "Threshold for bandos godsword (0 to disable)."
 	)
 	default int bandosGodswordThreshold()
 	{
@@ -170,7 +170,7 @@ public interface SpecialCounterConfig extends Config
 		position = 50,
 		keyName = "bulwarkThreshold",
 		name = "Dinh's bulwark",
-		description = "Threshold for Dinh's Bulwark (0 to disable)"
+		description = "Threshold for dinh's bulwark (0 to disable)."
 	)
 	default int bulwarkThreshold()
 	{

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/specialcounter/SpecialCounterConfig.java
@@ -38,7 +38,7 @@ public interface SpecialCounterConfig extends Config
 		position = 0,
 		keyName = "thresholdNotification",
 		name = "Threshold notifications",
-		description = "Sends a notification when your special attack counter exceeds the threshold"
+		description = "Sends a notification when your special attack counter exceeds the threshold."
 	)
 	default Notification thresholdNotification()
 	{
@@ -49,7 +49,7 @@ public interface SpecialCounterConfig extends Config
 		position = 1,
 		keyName = "specDrops",
 		name = "Spec drops",
-		description = "Draws an overlay over the player when a special attack hits"
+		description = "Draws an overlay over the player when a special attack hits."
 	)
 	default boolean specDrops()
 	{
@@ -60,7 +60,7 @@ public interface SpecialCounterConfig extends Config
 		position = 1,
 		keyName = "specDropMisses",
 		name = "Spec drop misses",
-		description = "Draws an overlay over the player when a special attack misses"
+		description = "Draws an overlay over the player when a special attack misses."
 	)
 	default boolean specDropMisses()
 	{
@@ -71,7 +71,7 @@ public interface SpecialCounterConfig extends Config
 		position = 2,
 		keyName = "specDropColor",
 		name = "Spec drop color",
-		description = "Text color for spec drops"
+		description = "Text color for spec drops."
 	)
 	default Color specDropColor()
 	{
@@ -82,7 +82,7 @@ public interface SpecialCounterConfig extends Config
 		position = 3,
 		keyName = "infobox",
 		name = "Infobox",
-		description = "Adds an infobox counting special attacks"
+		description = "Adds an infobox counting special attacks."
 	)
 	default boolean infobox()
 	{
@@ -93,7 +93,7 @@ public interface SpecialCounterConfig extends Config
 		position = 3,
 		keyName = "defenceDrainInfobox",
 		name = "Show defence drain infobox",
-		description = "If infoboxes are enabled, adds a defence drain percentage infobox for Dragon warhammer and Elder maul"
+		description = "If infoboxes are enabled, adds a defence drain percentage infobox for Dragon warhammer and Elder maul."
 	)
 	default boolean defenceDrainInfobox()
 	{
@@ -104,7 +104,7 @@ public interface SpecialCounterConfig extends Config
 		position = 10,
 		keyName = "dragonWarhammerThreshold",
 		name = "Dragon warhammer",
-		description = "Threshold for Dragon Warhammer (0 to disable)"
+		description = "Threshold for Dragon warhammer (0 to disable)."
 	)
 	default int dragonWarhammerThreshold()
 	{
@@ -115,7 +115,7 @@ public interface SpecialCounterConfig extends Config
 		position = 15,
 		keyName = "elderMaulThreshold",
 		name = "Elder maul",
-		description = "Threshold for Elder Maul (0 to disable)"
+		description = "Threshold for Elder maul (0 to disable)."
 	)
 	default int elderMaulThreshold()
 	{
@@ -126,7 +126,7 @@ public interface SpecialCounterConfig extends Config
 		position = 20,
 		keyName = "arclightThreshold",
 		name = "Arclight",
-		description = "Threshold for Arclight (0 to disable)"
+		description = "Threshold for Arclight (0 to disable)."
 	)
 	default int arclightThreshold()
 	{
@@ -137,7 +137,7 @@ public interface SpecialCounterConfig extends Config
 		position = 30,
 		keyName = "darklightThreshold",
 		name = "Darklight",
-		description = "Threshold for Darklight (0 to disable)"
+		description = "Threshold for Darklight (0 to disable)."
 	)
 	default int darklightThreshold()
 	{
@@ -148,7 +148,7 @@ public interface SpecialCounterConfig extends Config
 		position = 31,
 		keyName = "emberlightThreshold",
 		name = "Emberlight",
-		description = "Threshold for Emberlight (0 to disable)"
+		description = "Threshold for Emberlight (0 to disable)."
 	)
 	default int emberlightThreshold()
 	{
@@ -159,7 +159,7 @@ public interface SpecialCounterConfig extends Config
 		position = 40,
 		keyName = "bandosGodswordThreshold",
 		name = "Bandos godsword",
-		description = "Threshold for Bandos Godsword (0 to disable)"
+		description = "Threshold for Bandos godsword (0 to disable)."
 	)
 	default int bandosGodswordThreshold()
 	{
@@ -170,7 +170,7 @@ public interface SpecialCounterConfig extends Config
 		position = 50,
 		keyName = "bulwarkThreshold",
 		name = "Dinh's bulwark",
-		description = "Threshold for Dinh's Bulwark (0 to disable)"
+		description = "Threshold for Dinh's bulwark (0 to disable)."
 	)
 	default int bulwarkThreshold()
 	{