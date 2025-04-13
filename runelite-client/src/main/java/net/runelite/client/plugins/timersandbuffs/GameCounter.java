arbahb/ulu-p

timers buffs: add stone of jas empowerment

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/GameCounter.java
@@ -30,6 +30,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.runelite.api.ItemID;
+import net.runelite.api.NullItemID;
 import net.runelite.api.SpriteID;
 
 @Getter(AccessLevel.PACKAGE)
@@ -39,6 +40,7 @@ enum GameCounter
 	COLOSSEUM_DOOM(SpriteID.COLOSSEUM_DOOM, GameTimerImageType.SPRITE, "Doom"),
 	CURSE_OF_THE_MOONS_BLUE(ItemID.BLUE_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Blue Moon)", ColorBoundaryType.GREATER_THAN_EQUAL_TO, 18, Color.RED),
 	CURSE_OF_THE_MOONS_ECLIPSE(ItemID.ECLIPSE_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Eclipse Moon)"),
+	STONE_OF_JAS_EMPOWERMENT(NullItemID.NULL_29559, GameTimerImageType.ITEM, "Stone of Jas empowerment", false),
 	VENGEANCE_ACTIVE(SpriteID.SPELL_VENGEANCE_OTHER, GameTimerImageType.SPRITE, "Vengeance active", false),
 	;
 

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -615,6 +615,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarTimer(MOONLIGHT_POTION, moonlightValue, IntUnaryOperator.identity());
 		}
 
+		if (event.getVarbitId() == Varbits.STONE_OF_JAS_EMPOWERMENT && config.showOverload())
+		{
+			updateVarCounter(STONE_OF_JAS_EMPOWERMENT, event.getValue());
+		}
+
 		if (event.getVarbitId() == Varbits.BUFF_GOADING_POTION && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -661,6 +666,7 @@ public class TimersAndBuffsPlugin extends Plugin
 			removeGameTimer(OVERLOAD);
 			removeGameTimer(OVERLOAD_RAID);
 			removeGameTimer(SMELLING_SALTS);
+			removeVarCounter(STONE_OF_JAS_EMPOWERMENT);
 		}
 
 		if (!config.showPrayerEnhance())

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/GameCounter.java
@@ -39,6 +39,7 @@ enum GameCounter
 	COLOSSEUM_DOOM(SpriteID.COLOSSEUM_DOOM, GameTimerImageType.SPRITE, "Doom"),
 	CURSE_OF_THE_MOONS_BLUE(ItemID.FROST_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Blue Moon)", ColorBoundaryType.GREATER_THAN_EQUAL_TO, 18, Color.RED),
 	CURSE_OF_THE_MOONS_ECLIPSE(ItemID.ECLIPSE_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Eclipse Moon)"),
+	STONE_OF_JAS_EMPOWERMENT(ItemID.WGS_STONE_OF_JAS_DUMMY_ITEM, GameTimerImageType.ITEM, "Stone of Jas empowerment", false),
 	VENGEANCE_ACTIVE(SpriteID.SPELL_VENGEANCE_OTHER, GameTimerImageType.SPRITE, "Vengeance active", false),
 	;
 

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -613,6 +613,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarTimer(MOONLIGHT_POTION, moonlightValue, IntUnaryOperator.identity());
 		}
 
+		if (event.getVarbitId() == VarbitID.WGS_STONE_OF_JAS_BOOST && config.showTormentedDemonBuffs())
+		{
+			updateVarCounter(STONE_OF_JAS_EMPOWERMENT, event.getValue());
+		}
+
 		if (event.getVarbitId() == VarbitID.GOADING_POTION_TIMER && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -664,6 +669,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			removeGameTimer(OVERLOAD);
 			removeGameTimer(OVERLOAD_RAID);
 			removeGameTimer(SMELLING_SALTS);
+		}
+
+		if (!config.showTormentedDemonBuffs())
+		{
+			removeVarCounter(STONE_OF_JAS_EMPOWERMENT);
 		}
 
 		if (!config.showPrayerEnhance())