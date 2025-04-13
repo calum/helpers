arbahb/ulu-p

timers buffs: add burn damage

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/GameCounter.java
@@ -37,6 +37,8 @@ import net.runelite.api.SpriteID;
 @AllArgsConstructor
 enum GameCounter
 {
+	BURN_DAMAGE_ACCUMULATED(SpriteID.BURN_DAMAGE, GameTimerImageType.SPRITE, "Burn damage accumulated"),
+	BURN_DAMAGE_NEXT_HIT(ItemID.BURNING_CLAW, GameTimerImageType.ITEM, "Burn damage next hit"),
 	COLOSSEUM_DOOM(SpriteID.COLOSSEUM_DOOM, GameTimerImageType.SPRITE, "Doom"),
 	CURSE_OF_THE_MOONS_BLUE(ItemID.BLUE_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Blue Moon)", ColorBoundaryType.GREATER_THAN_EQUAL_TO, 18, Color.RED),
 	CURSE_OF_THE_MOONS_ECLIPSE(ItemID.ECLIPSE_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Eclipse Moon)"),

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -620,6 +620,16 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarCounter(STONE_OF_JAS_EMPOWERMENT, event.getValue());
 		}
 
+		if (event.getVarbitId() == Varbits.BURN_DAMAGE_ACCUMULATED && config.showBurnDamageAccumulated())
+		{
+			updateVarCounter(BURN_DAMAGE_ACCUMULATED, event.getValue());
+		}
+
+		if (event.getVarbitId() == Varbits.BURN_DAMAGE_NEXT_HIT && config.showBurnDamageNextHit())
+		{
+			updateVarCounter(BURN_DAMAGE_NEXT_HIT, event.getValue());
+		}
+
 		if (event.getVarbitId() == Varbits.BUFF_GOADING_POTION && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -841,6 +851,16 @@ public class TimersAndBuffsPlugin extends Plugin
 		if (!config.showMoonlightPotion())
 		{
 			removeVarTimer(MOONLIGHT_POTION);
+		}
+
+		if (!config.showBurnDamageAccumulated())
+		{
+			removeVarCounter(BURN_DAMAGE_ACCUMULATED);
+		}
+
+		if (!config.showBurnDamageNextHit())
+		{
+			removeVarCounter(BURN_DAMAGE_NEXT_HIT);
 		}
 
 		if (!config.showGoading())

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/GameCounter.java
@@ -36,6 +36,8 @@ import net.runelite.api.gameval.ItemID;
 @AllArgsConstructor
 enum GameCounter
 {
+	BURN_DAMAGE_ACCUMULATED(SpriteID.BURN_DAMAGE, GameTimerImageType.SPRITE, "Burn damage accumulated"),
+	BURN_DAMAGE_NEXT_HIT(ItemID.BONE_CLAW, GameTimerImageType.ITEM, "Burn damage next hit"),
 	COLOSSEUM_DOOM(SpriteID.COLOSSEUM_DOOM, GameTimerImageType.SPRITE, "Doom"),
 	CURSE_OF_THE_MOONS_BLUE(ItemID.FROST_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Blue Moon)", ColorBoundaryType.GREATER_THAN_EQUAL_TO, 18, Color.RED),
 	CURSE_OF_THE_MOONS_ECLIPSE(ItemID.ECLIPSE_MOON_HELM, GameTimerImageType.ITEM, "Curse of the Moons (Eclipse Moon)"),

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -618,6 +618,16 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarCounter(STONE_OF_JAS_EMPOWERMENT, event.getValue());
 		}
 
+		if (event.getVarbitId() == VarbitID.PLAYER_OWN_BURN_DAMAGE_TOTAL && config.showBurnDamageAccumulated())
+		{
+			updateVarCounter(BURN_DAMAGE_ACCUMULATED, event.getValue());
+		}
+
+		if (event.getVarbitId() == VarbitID.PLAYER_OWN_BURN_DAMAGE_HITSPLAT_SIZE && config.showBurnDamageNextHit())
+		{
+			updateVarCounter(BURN_DAMAGE_NEXT_HIT, event.getValue());
+		}
+
 		if (event.getVarbitId() == VarbitID.GOADING_POTION_TIMER && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -848,6 +858,16 @@ public class TimersAndBuffsPlugin extends Plugin
 		if (!config.showMoonlightPotion())
 		{
 			removeVarTimer(MOONLIGHT_POTION);
+		}
+
+		if (!config.showBurnDamageAccumulated())
+		{
+			removeVarCounter(BURN_DAMAGE_ACCUMULATED);
+		}
+
+		if (!config.showBurnDamageNextHit())
+		{
+			removeVarCounter(BURN_DAMAGE_NEXT_HIT);
 		}
 
 		if (!config.showGoading())