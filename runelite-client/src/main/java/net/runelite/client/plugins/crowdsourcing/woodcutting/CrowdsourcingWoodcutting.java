runelite/runelite

crowdsourcing: woodcutting: add trailblazer reloaded tools
Co-authored-by: Macweese <50101641+Macweese@users.noreply.github.com>

runelite-client/src/main/java/net/runelite/client/plugins/crowdsourcing/woodcutting/CrowdsourcingWoodcutting.java
@@ -146,7 +146,10 @@ public class CrowdsourcingWoodcutting
 		put(AnimationID.WOODCUTTING_RUNE, ItemID.RUNE_AXE).
 		put(AnimationID.WOODCUTTING_DRAGON, ItemID.DRAGON_AXE).
 		put(AnimationID.WOODCUTTING_DRAGON_OR, ItemID.DRAGON_AXE_OR).
+		put(AnimationID.WOODCUTTING_TRAILBLAZER_RELOADED, ItemID.DRAGON_AXE_OR_30352).
 		put(AnimationID.WOODCUTTING_INFERNAL, ItemID.INFERNAL_AXE).
+		put(AnimationID.WOODCUTTING_INFERNAL_TRAILBLAZER, ItemID.INFERNAL_AXE_OR).
+		put(AnimationID.WOODCUTTING_INFERNAL_TRAILBLAZER_RELOADED, ItemID.INFERNAL_AXE_OR_30347).
 		put(AnimationID.WOODCUTTING_3A_AXE, ItemID._3RD_AGE_AXE).
 		put(AnimationID.WOODCUTTING_CRYSTAL, ItemID.CRYSTAL_AXE).
 		put(AnimationID.WOODCUTTING_TRAILBLAZER, ItemID.INFERNAL_AXE_OR).build();

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/crowdsourcing/woodcutting/CrowdsourcingWoodcutting.java
@@ -146,7 +146,10 @@ public class CrowdsourcingWoodcutting
 		put(AnimationID.HUMAN_WOODCUTTING_RUNE_AXE, ItemID.RUNE_AXE).
 		put(AnimationID.HUMAN_WOODCUTTING_DRAGON_AXE, ItemID.DRAGON_AXE).
 		put(AnimationID.HUMAN_WOODCUTTING_TRAILBLAZER_AXE_NO_INFERNAL, ItemID.TRAILBLAZER_AXE_NO_INFERNAL).
+		put(AnimationID.HUMAN_WOODCUTTING_TRAILBLAZER_RELOADED_AXE_NO_INFERNAL, ItemID.TRAILBLAZER_RELOADED_AXE_NO_INFERNAL).
 		put(AnimationID.HUMAN_WOODCUTTING_INFERNAL_AXE, ItemID.INFERNAL_AXE).
+		put(AnimationID.HUMAN_WOODCUTTING_TRAILBLAZER_AXE, ItemID.TRAILBLAZER_AXE).
+		put(AnimationID.HUMAN_WOODCUTTING_TRAILBLAZER_RELOADED_AXE, ItemID.TRAILBLAZER_RELOADED_AXE).
 		put(AnimationID.HUMAN_WOODCUTTING_3A_AXE, ItemID._3A_AXE).
 		put(AnimationID.HUMAN_WOODCUTTING_CRYSTAL_AXE, ItemID.CRYSTAL_AXE).
 		put(AnimationID.HUMAN_OPENHEAVYCHEST, ItemID.TRAILBLAZER_AXE).build();