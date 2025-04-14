runelite/runelite

clues: add trailblazer reloaded tools
Co-authored-by: Macweese <50101641+Macweese@users.noreply.github.com>

runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/SkillChallengeClue.java
@@ -85,10 +85,13 @@ public class SkillChallengeClue extends ClueScroll implements NpcClueScroll, Nam
 		item(ItemID.DRAGON_PICKAXE_12797),
 		item(ItemID.DRAGON_PICKAXE_OR),
 		item(ItemID.DRAGON_PICKAXE_OR_25376),
+		item(ItemID.DRAGON_PICKAXE_OR_30351),
 		item(ItemID.INFERNAL_PICKAXE),
 		item(ItemID.INFERNAL_PICKAXE_OR),
+		item(ItemID.INFERNAL_PICKAXE_OR_30345),
 		item(ItemID.INFERNAL_PICKAXE_UNCHARGED),
 		item(ItemID.INFERNAL_PICKAXE_UNCHARGED_25369),
+		item(ItemID.INFERNAL_PICKAXE_UNCHARGED_30346),
 		item(ItemID.GILDED_PICKAXE),
 		item(ItemID._3RD_AGE_PICKAXE),
 		item(ItemID.CRYSTAL_PICKAXE),
@@ -113,11 +116,14 @@ public class SkillChallengeClue extends ClueScroll implements NpcClueScroll, Nam
 		item(ItemID.RUNE_FELLING_AXE),
 		item(ItemID.DRAGON_AXE),
 		item(ItemID.DRAGON_AXE_OR),
+		item(ItemID.DRAGON_AXE_OR_30352),
 		item(ItemID.DRAGON_FELLING_AXE),
 		item(ItemID.INFERNAL_AXE),
 		item(ItemID.INFERNAL_AXE_OR),
+		item(ItemID.INFERNAL_AXE_OR_30347),
 		item(ItemID.INFERNAL_AXE_UNCHARGED),
 		item(ItemID.INFERNAL_AXE_UNCHARGED_25371),
+		item(ItemID.INFERNAL_AXE_UNCHARGED_30348),
 		item(ItemID.GILDED_AXE),
 		item(ItemID._3RD_AGE_AXE),
 		item(ItemID._3RD_AGE_FELLING_AXE),
@@ -133,10 +139,13 @@ public class SkillChallengeClue extends ClueScroll implements NpcClueScroll, Nam
 		item(ItemID.BARBTAIL_HARPOON),
 		item(ItemID.DRAGON_HARPOON),
 		item(ItemID.DRAGON_HARPOON_OR),
+		item(ItemID.DRAGON_HARPOON_OR_30349),
 		item(ItemID.INFERNAL_HARPOON),
 		item(ItemID.INFERNAL_HARPOON_OR),
+		item(ItemID.INFERNAL_HARPOON_OR_30342),
 		item(ItemID.INFERNAL_HARPOON_UNCHARGED),
 		item(ItemID.INFERNAL_HARPOON_UNCHARGED_25367),
+		item(ItemID.INFERNAL_HARPOON_UNCHARGED_30343),
 		item(ItemID.CRYSTAL_HARPOON),
 		item(ItemID.CRYSTAL_HARPOON_INACTIVE),
 		item(ItemID.ECHO_HARPOON)

calum/helpers

runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/SkillChallengeClue.java
@@ -85,10 +85,13 @@ public class SkillChallengeClue extends ClueScroll implements NpcClueScroll, Nam
 		item(ItemID.DRAGON_PICKAXE_PRETTY),
 		item(ItemID.ZALCANO_PICKAXE),
 		item(ItemID.TRAILBLAZER_PICKAXE_NO_INFERNAL),
+		item(ItemID.TRAILBLAZER_RELOADED_PICKAXE_NO_INFERNAL),
 		item(ItemID.INFERNAL_PICKAXE),
 		item(ItemID.TRAILBLAZER_PICKAXE),
+		item(ItemID.TRAILBLAZER_RELOADED_PICKAXE),
 		item(ItemID.INFERNAL_PICKAXE_EMPTY),
 		item(ItemID.TRAILBLAZER_PICKAXE_EMPTY),
+		item(ItemID.TRAILBLAZER_RELOADED_PICKAXE_EMPTY),
 		item(ItemID.TRAIL_GILDED_PICKAXE),
 		item(ItemID._3A_PICKAXE),
 		item(ItemID.CRYSTAL_PICKAXE),
@@ -113,11 +116,14 @@ public class SkillChallengeClue extends ClueScroll implements NpcClueScroll, Nam
 		item(ItemID.RUNE_AXE_2H),
 		item(ItemID.DRAGON_AXE),
 		item(ItemID.TRAILBLAZER_AXE_NO_INFERNAL),
+		item(ItemID.TRAILBLAZER_RELOADED_AXE_NO_INFERNAL),
 		item(ItemID.DRAGON_AXE_2H),
 		item(ItemID.INFERNAL_AXE),
 		item(ItemID.TRAILBLAZER_AXE),
+		item(ItemID.TRAILBLAZER_RELOADED_AXE),
 		item(ItemID.INFERNAL_AXE_EMPTY),
 		item(ItemID.TRAILBLAZER_AXE_EMPTY),
+		item(ItemID.TRAILBLAZER_RELOADED_AXE_EMPTY),
 		item(ItemID.TRAIL_GILDED_AXE),
 		item(ItemID._3A_AXE),
 		item(ItemID._3A_AXE_2H),
@@ -133,10 +139,13 @@ public class SkillChallengeClue extends ClueScroll implements NpcClueScroll, Nam
 		item(ItemID.HUNTING_BARBED_HARPOON),
 		item(ItemID.DRAGON_HARPOON),
 		item(ItemID.TRAILBLAZER_HARPOON_NO_INFERNAL),
+		item(ItemID.TRAILBLAZER_RELOADED_HARPOON_NO_INFERNAL),
 		item(ItemID.INFERNAL_HARPOON),
 		item(ItemID.TRAILBLAZER_HARPOON),
+		item(ItemID.TRAILBLAZER_RELOADED_HARPOON),
 		item(ItemID.INFERNAL_HARPOON_EMPTY),
 		item(ItemID.TRAILBLAZER_HARPOON_EMPTY),
+		item(ItemID.TRAILBLAZER_RELOADED_HARPOON_EMPTY),
 		item(ItemID.CRYSTAL_HARPOON),
 		item(ItemID.CRYSTAL_HARPOON_INACTIVE),
 		item(ItemID.LEAGUE_TRAILBLAZER_HARPOON)